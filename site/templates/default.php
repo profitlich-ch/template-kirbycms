412740a3490448feb0c52416e3adf522a6770d2c
Moritz Profitlich <m.profitlich@profitlich.ch>
2025-07-07 17:23:42 +0200

    Kirby Update, Korrekturen

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,17 +1,3 @@
 <?php snippet('_template-top/template-top') ?>
-<?php snippet('header/header') ?>
-<?php snippet('intro/intro') ?>
-
-<main class="main" id="main">
-    <?php foreach ($page->children()->listed() as $part): ?>
-        <?php if ($part->abschnitte()->isNotEmpty()): ?>
-            <?php snippet('abschnitt/abschnitt', ['part' => $part]) ?>
-            <?php elseif ($part->form_fields()->isNotEmpty()): ?>
-                <?php snippet('bestellung/bestellung', ['part' => $part]) ?>
-        <?php endif ?>
-    <?php endforeach ?>
-</main>
-
-<?php snippet('footer/footer') ?>
-
+    INHALT
 <?php snippet('_template-bottom/template-bottom') ?>
