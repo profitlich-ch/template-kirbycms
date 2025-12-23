74861f67ac8403d125b10d0a054d05cf8ecf4043
Moritz Profitlich <m.profitlich@profitlich.ch>
2025-12-23 11:34:48 +0100

    Vite korrigiert, Gulp entfernt

--- site/config/vite.config.php
+++ site/config/vite.config.php
@@ -2,7 +2,7 @@
 // This is an auto-generated file. Please avoid making changes here.
 // Configure your settings in the "vite.config.js" file instead.
 return [
-  'outDir' => 'site/assets',
-  'assetsDir' => 'site/assets',
+  'outDir' => 'dist',
+  'assetsDir' => '',
   'legacy' => false
 ];
