ab27654f51393229d5f402917c073b18fc085427
Moritz Profitlich <m.profitlich@profitlich.ch>
2026-03-09 17:59:37 +0100

    Code aus Projekt PUR gelöscht

--- site/models/tour.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class TourPage extends Page {
-  public function isFreiePlaetze()
-    {
-        // return $this->freiePlaetze()->value === true ? "✅" : "❌";
-        return filter_var($this->freiePlaetze()->value, FILTER_VALIDATE_BOOLEAN) ? "✅" : "❌";
-    }
-}
